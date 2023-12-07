1e0f7833e2e0042eef29690f1747eb561bb837d9
Sheila Diz <dizsheila07@gmail.com>
2023-12-07 17:24:07 -0300

    style: change footer padding and font size

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,11 +3,11 @@ import { FaHeart, FaLinkedin, FaSquareGithub } from "react-icons/fa6"
 
 export default function Footer() {
   return (
-    <footer className="bg-red-300 py-5 px-4 flex flex-col md:flex-row justify-between align-middle mt-auto">
-			<p className="text-white m-0 text-md">
+    <footer className="bg-red-300 px-8 py-4 md:py-5 flex flex-col gap-2 md:flex-row justify-between align-middle mt-auto">
+			<p className="text-white m-0 text-md mt-0 md:mt-2">
         Copyright @2023 All rights reserved. | Made with <FaHeart className="inline-block"/> by Sheila Diz 
       </p>
-      <div id="socials" className="text-white text-3xl flex">
+      <div id="socials" className="text-white text-3xl md:text-4xl flex">
         <a href="https://github.com/sheidiz" target="_blank"> <FaSquareGithub /> </a>
         <a href="https://www.linkedin.com/in/sheila-abril-diz-308148191/" target="_blank"> <FaLinkedin /> </a>
       </div>
