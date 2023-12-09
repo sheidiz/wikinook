b48245e080dc3d6600588e9d7e48504e8ede4a6a
Sheila Diz <dizsheila07@gmail.com>
2023-12-09 02:58:20 -0300

    style: change pagination buttons size and border radius

--- src/app/(pages)/villagers/(components)/pagination.jsx
+++ src/app/(pages)/villagers/(components)/pagination.jsx
@@ -4,12 +4,12 @@ import ReactPaginate from "react-paginate";
 export default function Pagination({ pages, setCurrentPage }) {
   return (
     <ReactPaginate 
-      className="pagination my-4 flex justify-center gap2 md:gap-4"
+      className="pagination md:ms-48 my-4 flex justify-center gap-1 md:gap-2"
       previousLabel="Prev"
       nextLabel="Next"
-      previousClassName="p-2 btn bg-lightgreen text-white"
-      nextClassName="p-2 btn bg-lightgreen text-white"
-      pageClassName="p-2 btn bg-lightgreen opacity-700 text-white"
+      previousClassName="p-2 bg-lightgreen text-white text-sm rounded-md"
+      nextClassName="p-2 bg-lightgreen text-white text-sm rounded-md"
+      pageClassName="p-2 bg-lightgreen opacity-700 text-white text-sm rounded-md"
       pageLinkClassName=""
       activeClassName="active !bg-red-300"
       onPageChange={(data) =>{
