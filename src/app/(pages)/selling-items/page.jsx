ed19cebf5551c46538dae0c695d49ba296376ae8
Sheila Diz <dizsheila07@gmail.com>
2023-12-14 15:25:12 -0300

    style: change container border and padding

--- src/app/(pages)/selling-items/page.jsx
+++ src/app/(pages)/selling-items/page.jsx
@@ -13,12 +13,11 @@ export default function Page() {
     <section className="mt-3 md:mt-6 mb-8 md:mb-10 mx-2 md:mx-10 lg:mx-20">
       <SectionTitle>Selling List</SectionTitle>
       <SellingItemsTabs selection={selection} setSelection={setSelection} />
-      <div className="bg-white shadow border border-gray-100 p-2 md:p-8 text-gray-700 rounded-lg -mt-2 overflow-x-auto">
+      <div className=" mt-2 p-2 md:p-8 shadow border text-gray-700 rounded overflow-x-auto">
         {selection === "#bugs" && <BugsTab />}
         {selection === "#fish" && "Fish"}
         {selection === "#fossils" && "Fossils"}
         {selection === "#sea-creatures" && "Sea Creatures"}
-        
       </div>
 
     </section>
